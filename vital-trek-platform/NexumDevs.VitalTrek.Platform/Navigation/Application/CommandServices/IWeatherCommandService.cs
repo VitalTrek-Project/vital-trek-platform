f6f52316fc4936e0a24b6fd5acf360f8bc0c395e
GZ-99 <gabrielvilchezvite@gmail.com>
2026-06-15 15:08:55 -0500

    feat(application): add IExpeditionCommandService.cs, IExperienceCommandService.cs, IProgressCommandService.cs and IWeatherCommandService.cs

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/Navigation/Application/CommandServices/IWeatherCommandService.cs
@@ -0,0 +1,10 @@
+﻿using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Aggregates;
+using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Commands;
+using NexumDevs.VitalTrek.Platform.Shared.Application.Model;
+
+namespace NexumDevs.VitalTrek.Platform.Navigation.Application.CommandServices;
+
+public interface IWeatherCommandService
+{
+    Task<Result<Weather>> Handle(CreateWeatherCommand command, CancellationToken cancellationToken);
+}
