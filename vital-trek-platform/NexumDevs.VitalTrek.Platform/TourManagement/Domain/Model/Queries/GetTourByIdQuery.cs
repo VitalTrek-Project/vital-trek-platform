519699cf4cfbec49d826ec0865e67b4725596fa7
Miler <mileralexander2003@gmail.com>
2026-06-15 00:48:24 -0500

    feat: add gettourbyidquery for retrieving a tour by its unique identifier

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Domain/Model/Queries/GetTourByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace NexumDevs.VitalTrek.Platform.TourManagement.Domain.Model.Queries;
+
+/// <summary>
+/// Query used to retrieve a tour by its unique identifier.
+/// </summary>
+/// <param name="TourId">
+/// The unique identifier of the tour to retrieve.
+/// </param>
+public record GetTourByIdQuery(Guid TourId);
