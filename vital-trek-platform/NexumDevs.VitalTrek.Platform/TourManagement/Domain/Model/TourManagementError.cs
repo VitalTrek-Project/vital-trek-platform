a589f502473a0fd612b5f6d7aba866a3c6383a3a
Miler <mileralexander2003@gmail.com>
2026-06-15 00:59:07 -0500

    feat: enhance tourmanagementerror with detailed xml documentation and additional constructors

--- vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Domain/Model/TourManagementError.cs
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Domain/Model/TourManagementError.cs
@@ -1,20 +1,40 @@
 ﻿namespace NexumDevs.VitalTrek.Platform.TourManagement.Domain.Model;
 
 /// <summary>
-/// Excepción base del BC TourManagement. Encapsula una clave de error
-/// (definida en TourManagementErrors) que la capa de Interfaces puede
-/// traducir y mapear a un código HTTP / ProblemDetails.
-/// Equivalente a "PublishingError.cs".
+/// Represents the base exception for the TourManagement bounded context.
+/// Encapsulates an error code defined in <see cref="Errors.TourManagementErrors"/>
+/// that can be translated and mapped by the interface layer to HTTP status codes
+/// and ProblemDetails responses.
 /// </summary>
 public class TourManagementError : Exception
 {
+    /// <summary>
+    /// Gets the application-specific error code associated with the exception.
+    /// </summary>
     public string ErrorCode { get; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TourManagementError"/> class
+    /// using the specified error code as the exception message.
+    /// </summary>
+    /// <param name="errorCode">
+    /// The application-specific error code.
+    /// </param>
     public TourManagementError(string errorCode) : base(errorCode)
     {
         ErrorCode = errorCode;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TourManagementError"/> class
+    /// using the specified error code and custom message.
+    /// </summary>
+    /// <param name="errorCode">
+    /// The application-specific error code.
+    /// </param>
+    /// <param name="message">
+    /// The error message describing the exception.
+    /// </param>
     public TourManagementError(string errorCode, string message) : base(message)
     {
         ErrorCode = errorCode;
