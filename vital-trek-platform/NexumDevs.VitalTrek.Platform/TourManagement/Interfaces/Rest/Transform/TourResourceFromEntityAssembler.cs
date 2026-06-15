8ddd586a5d7592e569446dd7fbf4fc1e981348b1
Miler <mileralexander2003@gmail.com>
2026-06-14 21:17:42 -0500

    feat: add tourresourcefromentityassembler for converting tour entities to resources

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Interfaces/Rest/Transform/TourResourceFromEntityAssembler.cs
@@ -0,0 +1,18 @@
+﻿using NexumDevs.VitalTrek.Platform.TourManagement.Domain.Model.Aggregates;
+using NexumDevs.VitalTrek.Platform.TourManagement.Interfaces.Rest.Resources;
+
+namespace NexumDevs.VitalTrek.Platform.TourManagement.Interfaces.Rest.Transform;
+
+public static class TourResourceFromEntityAssembler
+{
+    public static TourResource ToResourceFromEntity(Tour entity)
+    {
+        return new TourResource(
+            entity.Id,
+            entity.Title,
+            entity.Description,
+            entity.Difficulty.ToString(),
+            entity.Status.ToString(),
+            entity.Capacity);
+    }
+}
