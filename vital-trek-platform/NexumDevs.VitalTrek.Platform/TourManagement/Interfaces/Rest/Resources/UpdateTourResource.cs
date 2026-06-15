0e8c2e8835c9f8d87870142de3f3dd3fbcbabfbd
Miler <mileralexander2003@gmail.com>
2026-06-15 01:15:03 -0500

    feat: add xml documentation for updatetourresource properties

--- vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Interfaces/Rest/Resources/UpdateTourResource.cs
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Interfaces/Rest/Resources/UpdateTourResource.cs
@@ -1,5 +1,14 @@
 ﻿namespace NexumDevs.VitalTrek.Platform.TourManagement.Interfaces.Rest.Resources;
 
+/// <summary>
+/// Represents the request resource used to update an existing tour.
+/// </summary>
+/// <param name="Title">
+/// The updated title of the tour.
+/// </param>
+/// <param name="Description">
+/// The updated description of the tour.
+/// </param>
 public record UpdateTourResource(
     string Title,
     string Description);
