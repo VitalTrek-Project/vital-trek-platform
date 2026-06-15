30ce12ebf36b39d1aafb555333cc913f00a0f612
Miler <mileralexander2003@gmail.com>
2026-06-15 00:45:58 -0500

    feat: add deletetourcommand for tour deletion functionality

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Domain/Model/Commands/DeleteTourCommand.cs
@@ -0,0 +1,11 @@
+﻿using NexumDevs.VitalTrek.Platform.TourManagement.Domain.Model.ValueObjects;
+
+namespace NexumDevs.VitalTrek.Platform.TourManagement.Domain.Model.Commands;
+
+/// <summary>
+/// Command used to delete an existing tour.
+/// </summary>
+/// <param name="TourId">
+/// The unique identifier of the tour to be deleted.
+/// </param>
+public record DeleteTourCommand(Guid TourId);
