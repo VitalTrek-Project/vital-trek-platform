4fe6cdc8720ab6b50000314646af6f21ce301497
GZ-99 <gabrielvilchezvite@gmail.com>
2026-06-15 16:10:52 -0500

    feat(application): add ExpeditionCommandService.cs, ExperienceCommandService.cs, ProgressCommandService.cs and WeatherCommandService.cs

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/Navigation/Application/Internal/CommandServices/ExperienceCommandService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using NexumDevs.VitalTrek.Platform.Navigation.Application.CommandServices;
+using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model;
+using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Aggregates;
+using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Commands;
+using NexumDevs.VitalTrek.Platform.Navigation.Domain.Repositories;
+using NexumDevs.VitalTrek.Platform.Resources.Errors;
+using NexumDevs.VitalTrek.Platform.Shared.Application.Model;
+using NexumDevs.VitalTrek.Platform.Shared.Domain.Repositories;
+
+namespace NexumDevs.VitalTrek.Platform.Navigation.Application.Internal.CommandServices;
+
+public class ExperienceCommandService(
+    IExperienceRepository experienceRepository,
+    IUnitOfWork unitOfWork,
+    IStringLocalizer<ErrorMessages> localizer,
+    ILogger<ExperienceCommandService> logger)
+     : IExperienceCommandService
+{
+    private readonly IStringLocalizer<ErrorMessages> _localizer = localizer;
+
+    public async Task<Result<Experience>> Handle(CreateExperienceCommand command, CancellationToken cancellationToken)
+    {
+        var experience = new Experience(command);
+
+        try
+        {
+            await experienceRepository.AddAsync(experience, cancellationToken);
+            await unitOfWork.CompleteAsync(cancellationToken);
+
+            return Result<Experience>.Success(experience);
+        }
+        catch (OperationCanceledException)
+        {
+            return Result<Experience>.Failure(NavigationError.OperationCancelled,
+                _localizer[nameof(NavigationError.OperationCancelled)]);
+        }
+        catch (DbUpdateException)
+        {
+            return Result<Experience>.Failure(NavigationError.DatabaseError,
+                _localizer[nameof(NavigationError.DatabaseError)]);
+        }
+        catch (Exception)
+        {
+            return Result<Experience>.Failure(NavigationError.InternalServerError,
+                _localizer[nameof(NavigationError.InternalServerError)]);
+        }
+    }
+}
