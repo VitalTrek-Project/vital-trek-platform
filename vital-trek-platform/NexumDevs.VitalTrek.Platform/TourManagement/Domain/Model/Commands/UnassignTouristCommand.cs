16f8def401b295426ccefc326db8587275072386
Miler <mileralexander2003@gmail.com>
2026-06-14 21:16:51 -0500

    feat: add UnassignTouristCommand record for unassigning tourists from tours

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Domain/Model/Commands/UnassignTouristCommand.cs
@@ -0,0 +1,5 @@
+﻿using NexumDevs.VitalTrek.Platform.TourManagement.Domain.Model.ValueObjects;
+namespace NexumDevs.VitalTrek.Platform.TourManagement.Domain.Model.Commands;
+
+public record UnassignTouristCommand(Guid TourId, Guid TouristId);
+
