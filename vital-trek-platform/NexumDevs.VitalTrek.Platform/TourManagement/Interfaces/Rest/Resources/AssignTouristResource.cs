2107077b7aa791b988029eb50ba4410c3188bb61
Miler <mileralexander2003@gmail.com>
2026-06-15 00:44:53 -0500

    feat: add assigntouristresource for assigning tourists to tours

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Interfaces/Rest/Resources/AssignTouristResource.cs
@@ -0,0 +1,9 @@
+﻿namespace NexumDevs.VitalTrek.Platform.TourManagement.Interfaces.Rest.Resources;
+
+/// <summary>
+/// Represents the request resource used to assign a tourist to a tour.
+/// </summary>
+/// <param name="TouristId">
+/// The unique identifier of the tourist to be assigned.
+/// </param>
+public record AssignTouristResource(Guid TouristId);
