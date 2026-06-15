c9c7ae12329b41d105cb8233e6fd05e190a006bc
Miler <mileralexander2003@gmail.com>
2026-06-15 01:13:47 -0500

    feat: add detailed xml documentation for tourresource properties

--- vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Interfaces/Rest/Resources/TourResource.cs
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Interfaces/Rest/Resources/TourResource.cs
@@ -1,5 +1,26 @@
 ﻿namespace NexumDevs.VitalTrek.Platform.TourManagement.Interfaces.Rest.Resources;
 
+/// <summary>
+/// Represents a tour resource returned by the API.
+/// </summary>
+/// <param name="Id">
+/// The unique identifier of the tour.
+/// </param>
+/// <param name="Title">
+/// The title of the tour.
+/// </param>
+/// <param name="Description">
+/// A detailed description of the tour.
+/// </param>
+/// <param name="Difficulty">
+/// The difficulty level of the tour.
+/// </param>
+/// <param name="Status">
+/// The current status of the tour.
+/// </param>
+/// <param name="Capacity">
+/// The maximum number of tourists allowed to participate in the tour.
+/// </param>
 public record TourResource(
     Guid Id,
     string Title,
