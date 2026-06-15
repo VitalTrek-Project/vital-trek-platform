dcae6e401c24759224dfe9e4030b0e1375222252
Miler <mileralexander2003@gmail.com>
2026-06-14 21:22:35 -0500

    feat: add touristassignedevent record for tour assignment events

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Domain/Model/Events/TouristAssignedEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace NexumDevs.VitalTrek.Platform.TourManagement.Domain.Model.Events;
+
+/// <summary>
+/// Evento de dominio que se dispara cuando un turista es asignado a un Tour.
+/// </summary>
+public record TouristAssignedEvent(Guid TourId, Guid TouristId, DateTimeOffset AssignedAt);
