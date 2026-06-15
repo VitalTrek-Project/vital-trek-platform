b19108395f4b6469620fa3d9148d759bd60fca07
Miler <mileralexander2003@gmail.com>
2026-06-14 21:17:59 -0500

    feat: add tourresource record for representing tour details

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Interfaces/Rest/Resources/TourResource.cs
@@ -0,0 +1,9 @@
+﻿namespace NexumDevs.VitalTrek.Platform.TourManagement.Interfaces.Rest.Resources;
+
+public record TourResource(
+    Guid Id,
+    string Title,
+    string Description,
+    string Difficulty,
+    string Status,
+    int Capacity);
