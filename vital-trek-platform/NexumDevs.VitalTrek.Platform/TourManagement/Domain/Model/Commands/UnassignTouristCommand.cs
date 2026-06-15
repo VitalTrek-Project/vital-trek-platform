bfc71a54f7b93c15e8f0ec79902e964b41ae94d7
Miler <mileralexander2003@gmail.com>
2026-06-15 01:14:27 -0500

    feat: add xml documentation for unassigntouristcommand parameters

--- vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Domain/Model/Commands/UnassignTouristCommand.cs
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Domain/Model/Commands/UnassignTouristCommand.cs
@@ -1,5 +1,16 @@
 ﻿using NexumDevs.VitalTrek.Platform.TourManagement.Domain.Model.ValueObjects;
+
 namespace NexumDevs.VitalTrek.Platform.TourManagement.Domain.Model.Commands;
 
-public record UnassignTouristCommand(Guid TourId, Guid TouristId);
-
+/// <summary>
+/// Command used to remove a tourist assignment from a tour.
+/// </summary>
+/// <param name="TourId">
+/// The unique identifier of the tour from which the tourist will be unassigned.
+/// </param>
+/// <param name="TouristId">
+/// The unique identifier of the tourist to unassign.
+/// </param>
+public record UnassignTouristCommand(
+    Guid TourId,
+    Guid TouristId);
