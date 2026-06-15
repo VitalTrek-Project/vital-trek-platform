1ef2966fd5f217c827eaea3970f68b10f22bd078
GZ-99 <gabrielvilchezvite@gmail.com>
2026-06-15 16:57:09 -0500

    feat(application): add ExpeditionQueryService.cs, ExperienceQueryService.cs, ProgressQueryService.cs and WeatherQueryService.cs

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/Navigation/Application/Internal/QueryServices/ExperienceQueryService.cs
@@ -0,0 +1,19 @@
+﻿using NexumDevs.VitalTrek.Platform.Navigation.Application.QueryServices;
+using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Aggregates;
+using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Queries;
+using NexumDevs.VitalTrek.Platform.Navigation.Domain.Repositories;
+
+namespace NexumDevs.VitalTrek.Platform.Navigation.Application.Internal.QueryServices;
+
+public class ExperienceQueryService(IExperienceRepository experienceRepository) : IExperienceQueryService
+{
+    public async Task<Experience?> Handle(GetExperienceByIdQuery query, CancellationToken cancellationToken)
+    {
+        return await experienceRepository.FindByIdAsync(query.ExperienceId, cancellationToken);
+    }
+    
+    public async Task<IEnumerable<Experience>> Handle(GetAllExperiencesQuery query, CancellationToken cancellationToken)
+    {
+        return await experienceRepository.ListAsync(cancellationToken);
+    }
+}
---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/Navigation/Application/Internal/QueryServices/WeatherQueryService.cs
@@ -0,0 +1,14 @@
+﻿using NexumDevs.VitalTrek.Platform.Navigation.Application.QueryServices;
+using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Aggregates;
+using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Queries;
+using NexumDevs.VitalTrek.Platform.Navigation.Domain.Repositories;
+
+namespace NexumDevs.VitalTrek.Platform.Navigation.Application.Internal.QueryServices;
+
+public class WeatherQueryService(IWeatherRepository weatherRepository) : IWeatherQueryService
+{
+    public async Task<Weather?> Handle(GetWeatherByIdQuery query, CancellationToken cancellationToken)
+    {
+        return await weatherRepository.FindByIdAsync(query.WeatherId, cancellationToken);
+    }
+}
