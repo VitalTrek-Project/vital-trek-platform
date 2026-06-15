2bbd6bb977828ec407281e24be664ca78007071f
Miler <mileralexander2003@gmail.com>
2026-06-15 00:56:29 -0500

    feat: enhance program.cs with tour management services and dependencies

--- vital-trek-platform/NexumDevs.VitalTrek.Platform/Program.cs
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/Program.cs
@@ -1,4 +1,3 @@
-
 using NexumDevs.VitalTrek.Platform.Resources.Errors;
 using NexumDevs.VitalTrek.Platform.Resources.Shared;
 using NexumDevs.VitalTrek.Platform.Shared.Domain.Repositories;
@@ -7,30 +6,30 @@ using NexumDevs.VitalTrek.Platform.Shared.Infrastructure.Mediator.Cortex.Configu
 using NexumDevs.VitalTrek.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
 using NexumDevs.VitalTrek.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
 using NexumDevs.VitalTrek.Platform.Shared.Infrastructure.Pipeline.Middleware.Extensions;
+using NexumDevs.VitalTrek.Platform.TourManagement.Resources;
 using Cortex.Mediator.Commands;
 using Cortex.Mediator.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
 using Microsoft.OpenApi;
-// Added for ProblemDetailsFactory
-// Added for base ProblemDetailsFactory
-// Added for IamMessages
-// Added for ProfilesMessages
+
+using NexumDevs.VitalTrek.Platform.TourManagement.Application.CommandServices;
+using NexumDevs.VitalTrek.Platform.TourManagement.Application.QueryService;
+using NexumDevs.VitalTrek.Platform.TourManagement.Application.Internal.CommandServices;
+using NexumDevs.VitalTrek.Platform.TourManagement.Application.Internal.QueryServices;
+using NexumDevs.VitalTrek.Platform.TourManagement.Domain.Repositories;
+using NexumDevs.VitalTrek.Platform.TourManagement.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+
 using ProblemDetailsFactory = NexumDevs.VitalTrek.Platform.Shared.Interfaces.Rest.ProblemDetails.ProblemDetailsFactory;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()))
     .AddDataAnnotationsLocalization();
 
-// Add ProblemDetails services
 builder.Services.AddProblemDetails();
 
-
-// Add CORS Policy
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAllPolicy",
@@ -39,9 +38,6 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader());
 });
 
-// Add Database Connection
-
-// Configure Database Context and route EF logs through the app logger pipeline.
 builder.Services.AddDbContext<AppDbContext>((serviceProvider, options) =>
 {
     var connectionStringTemplate = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -60,19 +56,13 @@ builder.Services.AddDbContext<AppDbContext>((serviceProvider, options) =>
         options.EnableSensitiveDataLogging();
 });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
-// Explicitly register IStringLocalizer for ErrorMessages and Commons
 builder.Services.AddSingleton<IStringLocalizer<ErrorMessages>, StringLocalizer<ErrorMessages>>();
-builder.Services
-    .AddSingleton<IStringLocalizer<CommonMessages>,
-        StringLocalizer<CommonMessages>>(); // Corrected from Common to Commons
+builder.Services.AddSingleton<IStringLocalizer<CommonMessages>, StringLocalizer<CommonMessages>>();
 
 
-// Register the custom ProblemDetailsFactory
 builder.Services.AddSingleton<ProblemDetailsFactory>();
-
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
@@ -109,32 +99,23 @@ builder.Services.AddSwaggerGen(options =>
     options.EnableAnnotations();
 });
 
-// Dependency Injection
-
-// Shared Bounded Context
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+builder.Services.AddScoped<ITourRepository, TourRepository>();
+builder.Services.AddScoped<ITourAssignmentRepository, TourAssignmentRepository>();
+builder.Services.AddScoped<IStringLocalizer>(sp =>
+    sp.GetRequiredService<IStringLocalizer<TourManagementMessages>>());
+builder.Services.AddSingleton<IStringLocalizer<TourManagementMessages>, StringLocalizer<TourManagementMessages>>();
+
+builder.Services.AddScoped<ITourCommandService, TourCommandService>();
+builder.Services.AddScoped<ITourQueryService, TourQueryService>();
 
 
-// TokenSettings Configuration
-
-//builder.Services.Configure<TokenSettings>(builder.Configuration.GetSection("TokenSettings"));
-
-
-
-// Mediator Configuration
-
-// Add Mediator Injection Configuration
 builder.Services.AddScoped(typeof(ICommandPipelineBehavior<>), typeof(LoggingCommandBehavior<>));
-
-// Add Cortex Mediator for Event Handling
-builder.Services.AddCortexMediator(
-    [typeof(Program)]);
-
+builder.Services.AddCortexMediator([typeof(Program)]);
 
 var app = builder.Build();
 
-// Apply pending migrations on startup (safe to call even when schema is up to date)
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
@@ -142,7 +123,6 @@ using (var scope = app.Services.CreateScope())
     context.Database.Migrate();
 }
 
-// Configure the HTTP request pipeline.
 app.UseGlobalExceptionHandler();
 
 var supportedCultures = new[] { "en", "es" };
@@ -159,11 +139,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// Apply CORS Policy
 app.UseCors("AllowAllPolicy");
-
-// Add Authorization Middleware to Pipeline
-// app.UseRequestAuthorization();
 
 app.UseHttpsRedirection();
 
