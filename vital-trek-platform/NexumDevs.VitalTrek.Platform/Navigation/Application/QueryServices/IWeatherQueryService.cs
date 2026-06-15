57a20efb8593213b5e6c7a32eae66ad90ab37fe5
GZ-99 <gabrielvilchezvite@gmail.com>
2026-06-15 16:27:00 -0500

    feat(application): add IExpeditionQueryService.cs, IExperienceQueryService.cs, IProgressQueryService.cs ans IWeatherQueryService.cs

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/Navigation/Application/QueryServices/IWeatherQueryService.cs
@@ -0,0 +1,9 @@
+﻿using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Aggregates;
+using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Queries;
+
+namespace NexumDevs.VitalTrek.Platform.Navigation.Application.QueryServices;
+
+public interface IWeatherQueryService
+{
+    Task<Weather?> Handle(GetWeatherByIdQuery query, CancellationToken cancellationToken);
+}
