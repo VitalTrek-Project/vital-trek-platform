14004352c70ddcc6c5b3fbca5c32e5f057ae7c9e
Miler <mileralexander2003@gmail.com>
2026-06-15 00:47:31 -0500

    feat: add getalltoursbyagencyquery for retrieving tours by agency

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Domain/Model/Queries/GetAllToursByAgencyQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace NexumDevs.VitalTrek.Platform.TourManagement.Domain.Model.Queries;
+
+/// <summary>
+/// Query used to retrieve all tours belonging to a specific agency.
+/// </summary>
+/// <param name="AgencyId">
+/// The unique identifier of the agency whose tours are being requested.
+/// </param>
+public record GetAllToursByAgencyQuery(Guid AgencyId);
