c628f2df053c7d80113552a788255a0f0a610de7
Miler <mileralexander2003@gmail.com>
2026-06-14 21:16:39 -0500

    feat: add UpdateTourCommand record for tour updates

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Domain/Model/Commands/UpdateTourCommand.cs
@@ -0,0 +1,7 @@
+﻿using NexumDevs.VitalTrek.Platform.TourManagement.Domain.Model.ValueObjects;
+namespace NexumDevs.VitalTrek.Platform.TourManagement.Domain.Model.Commands;
+
+public record UpdateTourCommand(
+    Guid TourId,
+    string Title,
+    string Description);
