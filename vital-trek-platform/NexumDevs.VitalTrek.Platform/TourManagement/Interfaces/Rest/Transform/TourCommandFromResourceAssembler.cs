bed6b7b8e22517c9b61e6e6c9e961b684793f7b4
Miler <mileralexander2003@gmail.com>
2026-06-14 21:23:22 -0500

    feat: add tourcommandfromresourceassembler for converting resource to commands

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/TourManagement/Interfaces/Rest/Transform/TourCommandFromResourceAssembler.cs
@@ -0,0 +1,27 @@
+﻿using NexumDevs.VitalTrek.Platform.TourManagement.Domain.Model.Commands;
+using NexumDevs.VitalTrek.Platform.TourManagement.Domain.Model.ValueObjects;
+using NexumDevs.VitalTrek.Platform.TourManagement.Interfaces.Rest.Resources;
+
+namespace NexumDevs.VitalTrek.Platform.TourManagement.Interfaces.Rest.Transform;
+
+public static class TourCommandFromResourceAssembler
+{
+    public static CreateTourCommand ToCreateCommandFromResource(CreateTourResource resource)
+    {
+        var difficulty = Enum.Parse<EDifficultyLevel>(resource.Difficulty, ignoreCase: true);
+
+        return new CreateTourCommand(
+            resource.AgencyId,
+            resource.Title,
+            resource.Description,
+            difficulty,
+            resource.Capacity,
+            resource.EstimatedDurationMinutes,
+            resource.DistanceKm);
+    }
+
+    public static UpdateTourCommand ToUpdateCommandFromResource(UpdateTourResource resource, Guid tourId)
+    {
+        return new UpdateTourCommand(tourId, resource.Title, resource.Description);
+    }
+}
