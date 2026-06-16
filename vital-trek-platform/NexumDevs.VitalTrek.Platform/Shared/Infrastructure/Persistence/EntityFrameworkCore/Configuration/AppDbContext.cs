69417601cbfa878fe3a36024529d7c9b77c6aef9
GZ-99 <gabrielvilchezvite@gmail.com>
2026-06-15 23:57:47 -0500

    feat(shared): update AppDbContext.cs

--- vital-trek-platform/NexumDevs.VitalTrek.Platform/Shared/Infrastructure/Persistence/EntityFrameworkCore/Configuration/AppDbContext.cs
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/Shared/Infrastructure/Persistence/EntityFrameworkCore/Configuration/AppDbContext.cs
@@ -2,6 +2,7 @@
 using NexumDevs.VitalTrek.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration.Extensions;
 using NexumDevs.VitalTrek.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Interceptors;
 using Microsoft.EntityFrameworkCore;
+using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Aggregates;
 
 namespace NexumDevs.VitalTrek.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
 
@@ -33,6 +34,58 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+        builder.Entity<Expedition>().HasKey(i => i.Id);
+        builder.Entity<Expedition>().Property(i => i.Id).IsRequired().ValueGeneratedOnAdd();
 
+        builder.Entity<Expedition>()
+            .Property(i => i.TourID).IsRequired();
+        builder.Entity<Expedition>()
+            .Property(i => i.GuideID).IsRequired();
+        builder.Entity<Expedition>()
+            .Property(i => i.Status).IsRequired();
+        builder.Entity<Expedition>()
+            .Property(i => i.StartedAt).IsRequired();
+        builder.Entity<Expedition>()
+            .Property(i => i.FinishedAt).IsRequired();
+        builder.UseSnakeCaseNamingConvention();
+        
+        builder.Entity<Experience>().HasKey(i => i.Id);
+        builder.Entity<Experience>().Property(i => i.Id).IsRequired().ValueGeneratedOnAdd();
+        
+        builder.Entity<Experience>()
+            .Property(i => i.ExpeditionID).IsRequired();
+        builder.Entity<Experience>()
+            .Property(i => i.TouristID).IsRequired();
+        builder.Entity<Experience>()
+            .Property(i => i.Note).IsRequired();
+        builder.Entity<Experience>()
+            .Property(i => i.MediaUrl).IsRequired();
+        builder.Entity<Experience>()
+            .Property(i => i.CreatedAt).IsRequired();
+        builder.UseSnakeCaseNamingConvention();
+        
+        builder.Entity<Progress>().HasKey(i => i.Id);
+        builder.Entity<Progress>().Property(i => i.Id).IsRequired().ValueGeneratedOnAdd();
+        
+        builder.Entity<Progress>()
+            .Property(i => i.CompletedCheckpoints).IsRequired();
+        builder.Entity<Progress>()
+            .Property(i => i.TotalCheckpoints).IsRequired();
+        builder.Entity<Progress>()
+            .Property(i => i.Percentage).IsRequired();
+        builder.UseSnakeCaseNamingConvention();
+        
+        builder.Entity<Weather>().HasKey(i => i.Id);
+        builder.Entity<Weather>().Property(i => i.Id).IsRequired().ValueGeneratedOnAdd();
+        
+        builder.Entity<Weather>()
+            .Property(i => i.TemperatureCelsius).IsRequired();
+        builder.Entity<Weather>()
+            .Property(i => i.Condition).IsRequired();
+        builder.Entity<Weather>()
+            .Property(i => i.Humidity).IsRequired();
+        builder.Entity<Weather>()
+            .Property(i => i.WindSpeedKmh).IsRequired();
+        builder.UseSnakeCaseNamingConvention();
     }
 }
