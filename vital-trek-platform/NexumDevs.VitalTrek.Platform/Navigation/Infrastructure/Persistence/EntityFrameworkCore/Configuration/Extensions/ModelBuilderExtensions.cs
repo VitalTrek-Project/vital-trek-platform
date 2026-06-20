bf20d1b6f2f3f9571b25f8922a79518cc35fb805
GZ-99 <gabrielvilchezvite@gmail.com>
2026-06-19 22:22:39 -0500

    feat(extensions): update ModelBuilderExtensions.cs

--- vital-trek-platform/NexumDevs.VitalTrek.Platform/Navigation/Infrastructure/Persistence/EntityFrameworkCore/Configuration/Extensions/ModelBuilderExtensions.cs
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/Navigation/Infrastructure/Persistence/EntityFrameworkCore/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -1,6 +1,7 @@
 using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Aggregates;
 using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Entities;
 using Microsoft.EntityFrameworkCore;
+using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.ValueObjects;
 
 namespace NexumDevs.VitalTrek.Platform.Navigation.Infrastructure.Persistence.EntityFrameworkCore.Configuration.Extensions;
 
