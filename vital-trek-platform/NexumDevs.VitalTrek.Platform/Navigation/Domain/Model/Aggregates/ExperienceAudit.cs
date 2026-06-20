c305617da6e8cb63e1e0a54ef59948e3e6aed6ef
GZ-99 <gabrielvilchezvite@gmail.com>
2026-06-19 21:06:43 -0500

    feat(experience): update ExperienceAudit.cs

--- vital-trek-platform/NexumDevs.VitalTrek.Platform/Navigation/Domain/Model/Aggregates/ExperienceAudit.cs
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/Navigation/Domain/Model/Aggregates/ExperienceAudit.cs
@@ -2,7 +2,7 @@
 
 namespace NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Aggregates;
 
-public partial class ExperienceAudit : IAuditableEntity
+public partial class Experience : IAuditableEntity
 {
     public DateTimeOffset? CreatedAt { get; set; }
     public DateTimeOffset? UpdatedAt { get; set; }
